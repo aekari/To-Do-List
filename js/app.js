87183515df48e704342a4efad0bd1ff78d760e83
dfsd <ace_e303@e303-s14.lab.lagcc>
2017-06-09 16:44:55 -0400

    added close function in js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,26 @@
 var userSubmit = document.querySelector('#user-submit');
+var toDoList = document.querySelector('#to-do-list');
 
 function addToDo(event){
 	event.preventDefault();
 
 	var userInput = document.querySelector('#user-input');
-	var toDoList = document.querySelector('#to-do-list')
 	if (userInput.value ==='') {
 		return false;
 	}
 
-	toDoList.innerHTML = '<li>' + userInput.value + '</li>' + toDoList.innerHTML;
+	toDoList.innerHTML = '<li><i class="fa fa-close-to-do fa-times-circle-o" aria-hidden="true"></i>'+ userInput.value + '</li>' + toDoList.innerHTML;
 
 	userInput.value = '';
 }
 
+function removeToDo(event){
+	// console.log(event.target.classList.contains('fa-close-to-do'));
+	if (event.target.classList.contains('fa-close-to-do')){
+		var li = event.target.parentElement;
+		toDoList.removeChild(li);
+	}
+}
+
+toDoList.addEventListener('click', removeToDo, false);
 userSubmit.addEventListener('click', addToDo, false)
